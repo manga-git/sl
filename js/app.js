5b5d5427b3bc198a2b4abbfc0a50a797f818c889
Ramakanth Manga <ramakanth.manga@gmail.com>
2014-08-11 21:21:55 -0500

    First Commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*
+$(document).ready(function(){
+	var img = document.getElementsByClassName('legalpad').firstChild;
+	img.onload = function() {
+	    if(img.height > img.width) {
+	        img.height = '100%';
+	        img.width = 'auto';
+	    }
+	};
+});
+*/
